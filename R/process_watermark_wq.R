cf1e88cd216fafcaefe516c9159cccb1497d1fbf
jakegross808 <jacob_gross@nps.gov>
2025-09-15 11:44:05 -1000

    added 'transect=="WQ"' back into process_watermark_wq function as this is needed to separate water quality from stream protocol. Streams will have a transect number ie "0 meteres", "15 meters", "30 meters".

--- R/process_watermark_wq.R
+++ R/process_watermark_wq.R
@@ -19,9 +19,12 @@ function(gdb_name, gdb_location, gdb_layer,park,loctype,
   t <-gdb_table_wq(gdb_name, gdb_location, gdb_layer)
 
   t_select <- t%>%
-    dplyr::filter(unit_code==park,Location_Type==loctype)%>%
+    dplyr::filter(unit_code==park,Location_Type==loctype,transect=="WQ")%>%
     dplyr::mutate(file_month = lubridate::month(created_date))%>% #create a month field to select on later
     dplyr::filter(file_month%in%select_months)
+
+  t_select_view <- t_select |>
+    mutate(DATA = blob::as_blob(DATA))
 
   apply(X = t_select, MARGIN = 1, FUN = watermark_wq, new_folder = output_folder)
 
